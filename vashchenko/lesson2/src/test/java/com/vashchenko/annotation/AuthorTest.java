1a4ac9c6f6b2ac94e5878fa37f086650074f003c
k.alinka555 <k.alinka555@list.ru>
2021-08-13 22:32:48 +0300

    vashchenko: lesson2

---
+++ vashchenko/lesson2/src/test/java/com/vashchenko/annotation/AuthorTest.java
@@ -0,0 +1,26 @@
+package com.vashchenko.annotation;
+
+import com.easyum.hibernate.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import static org.junit.Assert.*;
+
+public class AuthorTest {
+
+    public void testSavingAuthor(){
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Author author = new Author("Author");
+        session.persist(author);
+        session.
+        transaction.commit();
+        session.close();
+
+
+    }
+
+
+}
