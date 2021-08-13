ede7ca48cf1643fab6c528216112ffe0e72ee22c
antipov <anti123456.antipov@yandex.ru>
2021-08-13 22:26:41 +0300

    antipov:lesson2

---
+++ antipov/lesson2/src/main/java/com/antipov/hibernate/annotation/Author.java
@@ -0,0 +1,14 @@
+package com.antipov.hibernate.annotation;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column
+    private final String name;
+}
---
+++ antipov/lesson2/src/test/java/com/antipov/hibernate/annotation/AuthorTest.java
@@ -0,0 +1,23 @@
+package com.antipov.hibernate.annotation;
+
+import com.easyum.hibernate.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorTest {
+    @Test
+    public void testSavingAuthor() {
+        final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        final Session session = sessionFactory.openSession();
+        final Transaction transaction = session.beginTransaction();
+
+        final  Author author = new Author("Author");
+        session.persist(author);
+        transaction.commit();
+        session.close();
+    }
+}
