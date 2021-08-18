3482f982133910544e7b78f996eb720702ff8550
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2021-08-18 22:42:27 +0300

    esmagin: add simple crud

---
+++ esmagin/simplespringbootgradle/src/main/java/com/example/simplespringbootgradle/SimplespringbootgradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringbootgradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplespringbootgradleApplication.class, args);
+    }
+
+}
---
+++ esmagin/simplespringbootgradle/src/main/java/com/example/simplespringbootgradle/service/BookService.java
@@ -0,0 +1,15 @@
+package com.example.simplespringbootgradle.service;
+
+import com.example.simplespringbootgradle.model.Book;
+import com.example.simplespringbootgradle.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    public Book getBookById(Long id){
+        return bookRepository.findById(id).orElse( Book.builder().name("author").id(1l).build());
+    }
+}
